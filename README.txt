0d59d228bd82337f1f42f7dee616ba0a96518674
SantiDPM55 <133787224+SantiDPM55@users.noreply.github.com>
2023-06-30 17:36:39 -0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,9 @@
+La página va de la enseñanza de funciones básicas de HTML y CSS, el trabajo tuvo varios tropezones.
+Tuvimos problemas con la Base de Datos, como que la página de registro no le andaba el CSS, o no se enlazaba con la Base de Datos, hubo un caso del que no encontraba la página,
+o como hubo una enorme desorganización.
+Al principio se intentó usar Git Hub, pero después no lo usamos porque atrasaba demasiado, teniendo que invertir tiempo y esfuerzo en algo que no forma parte del proyecto.
+El uso de HTML y CSS no presento problemas, pero esto fue por conocimiento previos.
+Se usó PhpMyAdmin y JavaScript para la Base de Datos, mientras que PhpMyAdmin tenemos conocimientos previos, mientras que JavaScript nos faltó conocimiento,
+así que con algo de ayuda de los profes pudimos enlazarlo y crear una Base de Datos.
+Respecto al uso del aula, es demasiado incómoda, requiriendo mucho tiempo para entenderla, por lo general nosotros estamos acostumbrados a su uso,
+está sufre de muchos problemas respecto a otras ya usadas previamente.
---
+++ formularioserio.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-06-2023 a las 17:31:51
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 5.6.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `formularioserio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro`
+--
+
+CREATE TABLE `registro` (
+  `id` int(11) NOT NULL,
+  `Nombre` text COLLATE utf8_spanish_ci NOT NULL,
+  `Apellido` text COLLATE utf8_spanish_ci NOT NULL,
+  `Email` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `registro`
+--
+
+INSERT INTO `registro` (`id`, `Nombre`, `Apellido`, `Email`) VALUES
+(1, 'Santiago', 'Rebolledo', 'srebolledo@gmail.com'),
+(2, 'Martin', 'Rebolledo', 'srebolledo@gmail.com'),
+(3, 'Martin', 'Beccarini', 'srebolledo@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `registro`
+--
+ALTER TABLE `registro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
